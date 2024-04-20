e849c7b199dc696e9599c7efa0e133972dd7baf0
MuhamedAlili <118050595+MuhamedAlili@users.noreply.github.com>
2024-04-20 13:53:08 +0200

    asdadas

--- converter/convertapp/views.py
+++ converter/convertapp/views.py
@@ -1,28 +1,24 @@
 from django.shortcuts import render
-import sys
-from django.shortcuts import render
-import requests
-sys.setrecursionlimit(1500)
 
-def convert(request):
+CONVERSION_RATES = {
+    'USD': 1.0,
+    'EUR': 0.85,
+    'GBP': 0.73,
+}
+
+def convert_currency(request):
     if request.method == 'POST':
-        from_currency = request.POST.get('from_currency')
-        to_currency = request.POST.get('to_currency')
-        amount = float(request.POST.get('amount'))
-
-        # Make API request to fetch exchange rate
-        api_url = requests.get(f'https://api.coingecko.com/api/v3/simple/price?ids={from_currency}&vs_currencies={to_currency}')
-        response = requests.get(api_url)
-        if response.status_code == 200:
-            data = response.json()
-            conversion_rate = data[from_currency.lower()][to_currency.lower()]
-            converted_amount = float(amount) * conversion_rate
-        data = response.json()
-        rate = float(data['data']['rates'][to_currency])
-
-        converted_amount = amount * rate
-
-        return render(request, 'convertapp/convert.html', {'converted_amount': converted_amount})
-
-    return render(request, 'convertapp/convert.html')
-
+        amount = float(request.POST['amount'])
+        from_currency = request.POST['from_currency']
+        to_currency = request.POST['to_currency']
+        
+        converted_amount = amount * (CONVERSION_RATES[to_currency] / CONVERSION_RATES[from_currency])
+        
+        return render(request, 'currency_converter/result.html', {
+            'amount': amount,
+            'from_currency': from_currency,
+            'to_currency': to_currency,
+            'converted_amount': converted_amount,
+        })
+    else:
+        return render(request, 'currency_converter/converter.html', {'currencies': CONVERSION_RATES.keys()})
--- converter/converter/urls.py
+++ converter/converter/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+# currency_converter/urls.py
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('convert/', include('convertapp.urls')),
-    
+    path('', views.convertapp, name='convert_currency'),
 ]
+
